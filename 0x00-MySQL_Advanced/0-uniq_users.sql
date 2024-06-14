0b329e8c3a4d70cd994d9aed423b499958b4658c
mazzinno <abdennasr3@gmail.com>
2024-06-14 00:13:23 +0000

    task 1

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- creates table users
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
---
+++ 0x00-MySQL_Advanced/README.md
@@ -0,0 +1,1 @@
+this is 0x00-MySQL_Advanced
